cffd02c6a88556d026b82f2579e4ef7d438a5859
Jonathan Fields <77209635+jonofields@users.noreply.github.com>
2022-05-09 15:28:45 -0500

    Update to_bq.sh

--- to_bq.sh
+++ to_bq.sh
@@ -4,10 +4,10 @@ date=$(date '+%Y-%m-%d')
 bq mk \
 --table \
 --expiration 82800 \
-facebook_data.fb \
+<dataset>.fb \
 ~/scripts/schema.json
 
 bq load \
 --source_format=NEWLINE_DELIMITED_JSON \
-facebook_data.fb \
-gs://blended_ingestion/facebook/$date.json
+<dataset>.fb \
+gs://<bucket>/<object folder>/$date.json
